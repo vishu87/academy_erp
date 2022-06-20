db8a54fc4c5f7bb95cdc45e5f650348068d6b278
dipanshu <dipanshuchauhan23@gmail.com>
2022-06-20 16:25:43 +0530

    login page setup

--- resources/views/page_menu.blade.php
+++ resources/views/page_menu.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-	<div style="background:rgba(0,0,0,0.8); border-radius: 50%; height: 80px; width: 80px; display: inline-block; margin: 10px 0; overflow: hidden;display: flex;align-items: center;justify-content: center;">
+	<div style="background:rgba(0,0,0,0.8); border-radius: 50%; height: 80px; width: 80px; display: inline-block; margin: 10px auto; overflow: hidden;display: flex;align-items: center;justify-content: center;">
 		<img src="{{ url('assets/images/logo2x.png') }}" style="height:45px; width:auto;" />
 	</div>
 </div>
