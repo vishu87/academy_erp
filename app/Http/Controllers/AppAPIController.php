72d24ea7e386e082a7267e9873202f9d6ed93a86
pro <pro@pros-MacBook-Pro.local>
2022-07-04 13:51:22 +0530

    AppAPICtrl Signup API

--- app/Http/Controllers/AppAPIController.php
+++ app/Http/Controllers/AppAPIController.php
@@ -886,7 +886,7 @@ class AppAPIController extends Controller {
         $user = User::AuthenticateUser($request->header('apiToken'));
 
         $data['success'] = true;
-        $data['message'] = "Your profile will be removed in nex 24 hours, Thank You";
+        $data['message'] = "Your profile will be removed Within nex 24 hours.";
         return Response::json($data,200,array());
 
     }
